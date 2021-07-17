0e4ec9632ae6e6fed4ae5bf70ee6a09c1c5418a6
lepasq <pasquarelli.le@gmail.com>
2021-07-17 11:28:50 +0200

    Divide memory into sections

--- gb/memory.go
+++ gb/memory.go
@@ -1,13 +1,66 @@
 package gb
 
-var Data [0x10000]int32
-
-func ResetMemory() {
-	for i := range Data {
-		Data[i] = 0
-	}
+type Memory struct {
+	Cart  [0x8000]Word
+	VRAM  [0x2000]Word
+	ExRAM [0x2000]Word
+	WRAM  [0x1000]Word
+	// -- prohibited --
+	OAM [0xA0]Word
+	// -- unusable --
+	IO   [0x80]Word
+	HRAM [0x7E]Word
+	IE   Word // 0xFFFF
 }
 
-func Operator(address int32) int32 {
-	return Data[address]
+func (mem *Memory) ResetMemory() {
+	for i := range mem.Cart {
+		mem.Cart[i] = 0
+	}
+	for i := range mem.VRAM {
+		mem.VRAM[i] = 0
+	}
+	for i := range mem.ExRAM {
+		mem.ExRAM[i] = 0
+	}
+	for i := range mem.WRAM {
+		mem.WRAM[i] = 0
+	}
+	for i := range mem.OAM {
+		mem.OAM[i] = 0
+	}
+	for i := range mem.IO {
+		mem.IO[i] = 0
+	}
+	for i := range mem.HRAM {
+		mem.HRAM[i] = 0
+	}
+	mem.IE = 0
+}
+
+func (mem *Memory) Read(address int) Word {
+	switch {
+	case address < 0x8000:
+		return mem.Cart[address]
+	case address < 0xA000:
+		return mem.VRAM[address-0x8000]
+	case address < 0xC000:
+		return mem.ExRAM[address-0xA000]
+	case address < 0xE000:
+		return mem.WRAM[address-0xC000]
+	case address < 0xFE00:
+		return 0 // prohibited area
+	case address < 0xFEA0:
+		return mem.OAM[address-0xFE00]
+	case address < 0xFF00:
+		return 0 // unusable
+	case address < 0xFF80:
+		return mem.IO[address-0xFF00]
+	case address < 0xFFFF:
+		return mem.HRAM[address-0xFF80]
+	case address == 0xFFFF:
+		return mem.IE
+	default:
+		return 0
+	}
 }
